1b5180b4470fad16ffde63619225502c3dd15834
Ronen Wolfsberger <ronen.wolfsberger@gmail.com>
2026-03-28 15:48:11 +0300

    Add האח הגדול 2026 participants with photos

--- src/app/api/admin/shows/[id]/fetch-participants/route.ts
+++ src/app/api/admin/shows/[id]/fetch-participants/route.ts
@@ -3,6 +3,28 @@ import { requireAdmin } from '@/lib/auth';
 
 // Known participant data indexed by show name (Hebrew)
 const KNOWN_SHOWS: Record<string, { name: string; photo_url: string | null }[]> = {
+  'האח הגדול': [
+    { name: 'פאינה לזובסקי',    photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_450,w_450/1077439' },
+    { name: 'אלירן דוד ביטון',  photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_450,w_450/1077441' },
+    { name: 'שלי סרבריאניקוב',  photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_450,w_450/1077443' },
+    { name: 'עומר ציפורי',      photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_450,w_450/1077447' },
+    { name: 'מירה נואף',        photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077450' },
+    { name: 'זוהר אייזיק זקן',  photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_450,w_450/1077453' },
+    { name: 'פטל אסף',          photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077455' },
+    { name: 'תמיר גולן',        photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077458' },
+    { name: 'אור כהן',          photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077460' },
+    { name: 'נדב ברנס',         photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_450,w_450/1077467' },
+    { name: 'טל טיטו',          photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077469' },
+    { name: 'הילי לאה בצלאל',   photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077471' },
+    { name: 'נעם טקלה',         photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077473' },
+    { name: 'גאיה קלדרון',      photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077484' },
+    { name: 'הודיה כהן',        photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_450,w_450/1077684' },
+    { name: 'קורן חתוכה',       photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_450,w_450/1077686' },
+    { name: 'דניאל משה',        photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077696' },
+    { name: 'הילה חיטכס',       photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077699' },
+    { name: 'בן אוזלבו',        photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_711,w_400/1077704' },
+    { name: 'גל רובין',         photo_url: 'https://images.maariv.co.il/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_400,w_711/1078517' },
+  ],
   'המירוץ למיליון 2026': [
     { name: 'שלומי יפרח ואסף זגה',   photo_url: 'https://img.mako.co.il/2026/01/21/W_grid_asaf_shlomi132_autoOrient_w.jpg' },
     { name: 'חן דריקס ויוסי עזרא',   photo_url: 'https://img.mako.co.il/2026/03/25/W_grid_YOSSIVCHEN(1)_autoOrient_w.jpg' },
